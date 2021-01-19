410fad77f37caee8746cfe526ee1e1b0c63a7e32
tshuangteng@gmail.com <tshuangteng@gmail.com>
2021-01-19 15:59:08 +0800

    update cv2base64

--- python/pypi/file/for_img.py
+++ python/pypi/file/for_img.py
@@ -9,6 +9,15 @@ with open(str(img_path), 'rb') as f:
     base64_data = base64.b64encode(f.read()).decode('utf-8')
     body = {'img': base64_data}
 
+# 服务端处理
+try:
+    img_base64 = body['image']
+    decode_base64_data = base64.b64decode(img_base64)
+    img_array = np.frombuffer(decode_base64_data, np.uint8)
+    img = cv2.imdecode(img_array, cv2.COLOR_BGR2RGB)
+except Exception as e:
+    return ''
+
 # 服务端接收
 decode_base64_data = base64.b64decode(body['img'])
 
