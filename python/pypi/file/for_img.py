2ebfa666c75a2e8b0bc76aa8185a2e8a30b1646f
tshuangteng@gmail.com <tshuangteng@gmail.com>
2021-01-22 11:16:06 +0800

    image file cv2

--- python/pypi/file/for_img.py
+++ python/pypi/file/for_img.py
@@ -7,7 +7,7 @@ img_path = ''
 # 客户端请求
 with open(str(img_path), 'rb') as f:
     base64_data = base64.b64encode(f.read()).decode('utf-8')
-    body = {'img': base64_data}
+    body = {'image': base64_data}
 
 # 服务端处理
 try:
@@ -16,10 +16,15 @@ try:
     img_array = np.frombuffer(decode_base64_data, np.uint8)
     img = cv2.imdecode(img_array, cv2.COLOR_BGR2RGB)
 except Exception as e:
-    ''
+    pass
 
 # 服务端接收
 decode_base64_data = base64.b64decode(body['img'])
 
 img_array = np.fromstring(base64_data, np.uint8)
 img = cv2.imdecode(img_array, cv2.COLOR_BGR2RGB)
+
+# 还原图片
+img_data = base64.b64decode(body['image'])
+with open('origin.jpg', 'wb') as wf:
+    wf.write(img_data)
