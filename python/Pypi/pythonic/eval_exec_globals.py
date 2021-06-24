38ed4daeaa5377da16bae4af2ee34b35e8f4ec98
tshuangteng@gmail.com <tshuangteng@gmail.com>
2021-06-24 13:24:28 +0800

    python's eval locals globals

--- python/Pypi/pythonic/eval_exec_globals.py
+++ python/Pypi/pythonic/eval_exec_globals.py
@@ -1,29 +1,32 @@
 list1 = ['A', 'B', 'C', 'D']
-B = ['c']
 for i in list1:
-    locals()[i] = []
-    # globals()[i] = []
-    # exec(f"{i} = []")
-print(B)
+    locals()[i] = ['a']
+    # globals()[i] = ['b']
+    # exec(f"{i} = ['c']")
+print(A,B,C,D)
 print(locals())
-import math
+print(globals())
 
-# n = "[[1,2], [3,4], [5,6], [7,8], [9,0]]"
-# new = eval(n)
-# print(new)
-#
-# print(type(...))
-
-# for _ in range(100000):
-#     with open('a.txt', 'a', encoding='utf8') as f:
-#         f.write(f'{9}')
-#
-# s1 = input("请输入a+b:")
-# print(eval(s1))
-
-
+#####################################################
+# 1
 with open('a.txt', 'r', encoding='utf-8') as f:
     all_line_list = f.readlines()
     for line_list in all_line_list:
         ff = eval(line_list)
+        print(ff)
         print(type(ff))
+
+# 2
+# s1 = input("请输入a+b:")
+# print(eval(s1))
+
+# 3
+n = "[[1,2], [3,4], [5,6], [7,8], [9,0]]"
+new = eval(n)
+print(new)
+print(type(new))
+print(type(...))  # <class 'ellipsis'>
+
+# eval()  --->   evaluate arbitrary Python expressions from a string-based or compiled-code-based input
+# locals() --->  set current global scope or namespace
+# globals()  --->  set current global scope or namespace
