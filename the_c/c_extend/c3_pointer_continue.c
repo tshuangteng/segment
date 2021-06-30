27b915413bdd6f9d10c91b2959c06f987d747c63
tshuangteng@gmail.com <tshuangteng@gmail.com>
2021-06-30 22:33:49 +0800

    c pointer

--- the_c/c_extend/c3_pointer_continue.c
+++ the_c/c_extend/c3_pointer_continue.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-/* ---------- */
 
-int main()
-{
-    // 每一组的字节数相同来确保 内存对齐.
+int main() {
+    // 每一组的字节数相同来确保 内存中的对齐.
     // 延伸: 如果中间的一组字节数比较大, 则应该是从中间开始向2边对齐.
     typedef struct Info {
         char *name;  // 第一组: 8字节(*表示了要取该数据结构下的最大字节数)
         // char name[7];
-        int age; // 第二组: 8字节(以最大字节数)
-        double height;  // 第三组: 8字节 最大字节为单位
-        short hobby[4];  // 第四组: 8字节 最大字节为单位
+        int age; // 第二组: 8字节(以最大字节数表示)
+        double height;  // 第三组: 8字节 最大字节的单位
+        short hobby[4];  // 第四组: 8字节 最大字节的单位
     } info;
 
     // int info_size = sizeof(struct Info);
@@ -25,9 +23,9 @@ int main()
     // printf("*tp1 = %f", *tp1);
 
     // ht 数组名表示数组首元素的地址，是指针常量
-    double *tp2 = (double *) ((int)&ht + 8);  // 指针常量 强制转换为普通的int数值, 内存中是以16进制(16字节)来表示计算的, 接着加数值8也就是8字节, 才能移动到该数组中的第二个元素.
-    double *tp3 = (double *) ((char*)&ht + 16);  // 指针常量 强制转换为char类型的指针, 加16表示 移动16个sizeof(char)单位, 才能移动到该数组的第三个元素.
-    double *tp4 = (double *) ((int*)&ht + 2); // 指针常量 强制转换为int类型的指针, 加2表示 移动2个sizeof(int)单位, 才能移动到该数组的第二个元素.
+    double *tp2 = (double *) ((int) &ht + 8);  // 指针常量 强制转换为普通的int数值, 内存中是以16进制(16字节)来表示计算的, 接着加数值8也就是8字节, 才能移动到该数组中的第二个元素.
+    double *tp3 = (double *) ((char *) &ht + 16);  // 指针常量 强制转换为char类型的指针, 加16表示 移动16个sizeof(char)单位, 才能移动到该数组的第三个元素.
+    double *tp4 = (double *) ((int *) &ht + 2); // 指针常量 强制转换为int类型的指针, 加2表示 移动2个sizeof(int)单位, 才能移动到该数组的第二个元素.
 
     printf("*tp2 = %d\n", *tp2);
     printf("*tp3 = %f\n", *tp3);
