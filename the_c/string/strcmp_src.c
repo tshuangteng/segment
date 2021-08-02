587eba7c75ab04f5e0f45cb0f6861dcd92688f9c
tshuangteng@gmail.com <tshuangteng@gmail.com>
2021-08-02 17:26:59 +0800

    strcmp source code analyse

--- the_c/string/strcmp_src.c
+++ the_c/string/strcmp_src.c
@@ -1,47 +1,41 @@
-/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
+/* Copyright (C) 1991-2019 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
-
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
-   <https://www.gnu.org/licenses/>.  */
-
+   <http://www.gnu.org/licenses/>.  */
 #include <string.h>
-
 #undef strcmp
-
 #ifndef STRCMP
 # define STRCMP strcmp
 #endif
-
 /* Compare S1 and S2, returning less than, equal to or
    greater than zero if S1 is lexicographically less than,
    equal to or greater than S2.  */
+
+// c/c++中, 输出字符指针(指针指向的内容是字符)就是输出字符串,程序会自动在遇到'\0'后停止.
+
 int
-STRCMP (const char *p1, const char *p2)
+STRCMP (const char *p1, const char *p2)  // (const char) *p1 表示: 读取p1指向的类型的值,接着转换为const char. 如果p1是double*,则将读取double所有字节的内容,接着转换为const char. [*p1也表示完整的将实参传了进来]
 {
-  const unsigned char *s1 = (const unsigned char *) p1;
-  const unsigned char *s2 = (const unsigned char *) p2;
-  unsigned char c1, c2;
-
-  do
+    const unsigned char *s1 = (const unsigned char *) p1;  // 初始化一个常量无符号字符指针s1, 为了字符方便比较, 全都转换为无符号常量char指针.
+    const unsigned char *s2 = (const unsigned char *) p2;  // (const unsigned char *) p2 表示: 首先指针指向为p2, 然后读取p2中指向位置的第一个字节内容. 即使是double*也只读取第一个字节.
+    unsigned char c1, c2;
+    do
     {
-      c1 = (unsigned char) *s1++;
-      c2 = (unsigned char) *s2++;
-      if (c1 == '\0')
-	return c1 - c2;
+        c1 = (unsigned char) *s1++;  // 指针后移一位sizeof(char),也就是1字节到后一个字符处.
+        c2 = (unsigned char) *s2++;
+        if (c1 == '\0')  // 结束循环: 字符指针移动到s1字符串的结尾处'\0', 如果此时的c2还有字符时,相减为负; 如果此时的c2恰好也是'\0',相减为0.
+            return c1 - c2;
     }
-  while (c1 == c2);
-
-  return c1 - c2;
+    while (c1 == c2);  // 循环条件: 通过字符指针s1和s2的自增自减,移动到的字符都是一样的.
+    return c1 - c2;  // 结束循环: 字符指针移动到s2字符串的结尾处'\0', 而此时的c1还有字符,相减为正.
 }
 libc_hidden_builtin_def (strcmp)
--- the_c/string/strcmp_test.c
+++ the_c/string/strcmp_test.c
@@ -1,44 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 // https://stackoverflow.com/questions/20106732/c-difference-between-unsigned-char-s1-and-unsigned-chars1
-
-int demo_cmp(p1, p2)
-        const char *p1;
-        const char *p2;
-{
-    printf("%s\n", p1);
-    printf("%s\n\n", p2);
-
-    printf("%d\n", p1);
-    printf("%d\n\n", p2);
-
-    const unsigned char *s1 = (const unsigned char *) p1;
-    const unsigned char *s2 = (const unsigned char *) p2;
-//    const unsigned char *s3 = (const unsigned char) *p2;
-
-    printf("%s\n", s1);
-    printf("%c\n", *s1++);
-    printf("%c\n\n", *s1);
-
-    printf("%s\n", s2);
-    printf("%c\n", *s2++);
-    printf("%c\n\n", *s2);
-
-    return 0;
-}
-
 
 int main() {
 
-    char p1[] = "hello world";
-    char p2[] = "hello world";
-    //    int p1[] = {123};
-    //    int p2[] = {789};
-    printf("%s\n\n",p1);
+    char str1[] = "hello world";
+    char str2[] = "hello world";
+    printf("%s\n",str1);
+    printf("%s\n\n",str2);
 
-    printf("%d\n",*p1);
-    printf("%x\n",*p1);
-    printf("%d\n",*p2);
-    printf("%x\n",*p2);
+    const char *p1 = str1;
+    const char *p2 = str2;
+    printf("%s\n", p1);
+    printf("%s\n\n", p2);
+
+    const unsigned char *s1 = (const unsigned char *) p1;
+    const unsigned char *s2 = (const unsigned char *) p2;
+
+    printf("%s\n", s1);
+    printf("%s\n\n", s2);
+
+//    char test1[] = "hello world";
+//    char test2[] = "hello world";
+//    int res = strcmp(test1,test2);
+//    printf("%d\n\n", res);
+//
+//    int positive = 'd' - '\0';
+//    int negative = '\0' - 'd';
+//    printf("%d\n", positive);
+//    printf("%d\n", negative);
 
     return 0;
 }
