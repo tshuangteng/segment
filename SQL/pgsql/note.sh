c0f96db697218e478925da44bc018df62465df6a
tshuangteng <tshuangteng@gmail.com>
2021-12-02 09:56:57 +0800

    sql command

--- SQL/pgsql/note.sh
+++ SQL/pgsql/note.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# 查询表结构
+select COLUMN_NAME from information_schema.COLUMNS where table_name = "  ";
+
 
 # 复制表结构
-# create table addr_simulation (like addr_real INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES );
+# create table addr_simulation (like addr_real INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES);
 NOTICE:  table doesn't have 'DISTRIBUTED BY' clause, defaulting to distribution columns from LIKE table
 CREATE TABLE
 
@@ -11,8 +14,12 @@ https://blog.csdn.net/wlwlwlwl015/article/details/52493197
 
 # 新增字段
 alter table i1_import_raw add column receiverTownName text;
+alter table i1_import_raw add column receiverDetailAddress text null;
 alter table i1_import_raw add column receiverTownName text null;
+
 alter table i1_import_raw drop column receiverDetailAddress;
+alter table i1_import_raw drop column rnship;
+
 
 # 注意事项:
 首先添加默认值的字段,然后再给已经添加的字段添加默认值, 速度较快.
