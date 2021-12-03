b23192059bea5ec3740d9d452b89896ff4d0f811
tshuangteng <tshuangteng@gmail.com>
2021-12-03 15:01:46 +0800

    c

--- python/problems/common/notes.py
+++ python/problems/common/notes.py
@@ -1,4 +1,10 @@
 # 均分的切割l, 每一份的大小为n
+
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+express = [1,2,3,4,5]
+all_chunks = list(chunks(express, 2))
+for ep in all_chunks:
+    print(ep)
