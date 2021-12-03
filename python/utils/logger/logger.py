c550e917d468277256474ce320601f17692b31f4
tshuangteng <tshuangteng@gmail.com>
2021-12-03 13:55:14 +0800

    python file

--- python/utils/logger/logger.py
+++ python/utils/logger/logger.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 from pathlib import Path
-from python.common.constant import PROJECT_PATH
+from python.Pypi.file.constant import PROJECT_PATH
 from datetime import datetime
 
 
