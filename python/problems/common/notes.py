4e5801344e7ef0455afb5818aeea7f0a406eab44
tshuangteng <tshuangteng@gmail.com>
2021-12-27 11:10:26 +0800

    up

--- python/problems/common/notes.py
+++ python/problems/common/notes.py
@@ -4,7 +4,8 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-express = [1,2,3,4,5]
+
+express = [1, 2, 3, 4, 5]
 all_chunks = list(chunks(express, 2))
 for ep in all_chunks:
     print(ep)
